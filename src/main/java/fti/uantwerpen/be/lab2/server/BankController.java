952c37d95f225835a5e8aa01465e6fae0939df50
Matthias De Beukelaer <matthiasdb2912@gmail.com>
2023-03-21 16:37:18 +0100

    Created Client.java. REST uri's are now in line with naming conventions

--- src/main/java/fti/uantwerpen/be/lab2/server/BankController.java
+++ src/main/java/fti/uantwerpen/be/lab2/server/BankController.java
@@ -24,6 +24,7 @@ public class BankController {
      * @return A string
      */
     @GetMapping("/bank")
+    @ResponseBody
     public String welcome() {
         return "Welcome to this amazing test bank!!";
     }
@@ -34,38 +35,40 @@ public class BankController {
      * @param name The name of the account to get the balance of.
      * @return A Double
      */
-    @GetMapping("/account/getBalance") // /account/getBalance?name={name}
-    public double getBalance(@RequestParam String name) {
+    @GetMapping("/account/{name}/get-balance") // /account/getBalance?name={name}
+    @ResponseBody
+    public double getBalance(@PathVariable String name) {
         BankAccount account = repository.findByNames(name).orElseThrow(() -> new BankAccountNotFoundException(name));
         return account.getBalance();
     }
 
     /**
-     * If the account exists, add the amount to the balance and save the account.
+     * If the account exists, add money to the account with the given name.
      *
-     * @param info a map of the request parameters
-     * @return A string
+     * @param name The name of the account to add money to.
+     * @param amount The amount of money to add to the account.
+     * @return A String
      */
-    @PutMapping("/account/addMoney") // /account/addMoney?name={name}&amount={amount}
-    public String addMoney(@RequestParam Map<String, String> info) {
-        String name = info.get("name");
-        double amount = Double.parseDouble(info.get("amount"));
+    @PutMapping("/account/{name}/add-money/{amount}") // /account/addMoney?name={name}&amount={amount}
+    @ResponseBody
+    public String addMoney(@PathVariable String name, @PathVariable Double amount) {
         BankAccount account = repository.findByNames(name).orElseThrow(() -> new BankAccountNotFoundException(name));
         account.setBalance(account.getBalance() + amount);
         repository.save(account);
         return "New balance is " + account.getBalance();
     }
 
+
     /**
-     * If the account exists, remove the amount from the balance and save the account.
+     * If the account exists, remove money from the account with the given name.
      *
-     * @param info This is the name of the parameter that will be passed in.
-     * @return A string
+     * @param name The name of the account to remove money from.
+     * @param amount The amount of money to remove from the account.
+     * @return A String
      */
-    @PutMapping("/account/removeMoney") // /account/removeMoney?name={name}&amount={amount}
-    public String removeMoney(@RequestParam Map<String, String> info) {
-        String name = info.get("name");
-        double amount = Double.parseDouble(info.get("amount"));
+    @PutMapping("/account/{name}/remove-money/{amount}") // /account/removeMoney?name={name}&amount={amount}
+    @ResponseBody
+    public String removeMoney(@PathVariable String name, @PathVariable Double amount) {
         BankAccount account = repository.findByNames(name).orElseThrow(() -> new BankAccountNotFoundException(name));
         account.setBalance(account.getBalance() - amount);
         repository.save(account);
